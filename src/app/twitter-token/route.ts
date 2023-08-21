225d81ba6ed301a93da7b080db4fc9829cab3d43
MikuroXina <ryosukadnak@gmail.com>
2023-08-21 17:07:24 +0900

    Check TWITTER_CLIENT_SECRET

--- src/app/twitter-token/route.ts
+++ src/app/twitter-token/route.ts
@@ -2,6 +2,12 @@ import { TWITTER_CLIENT_ID } from "@/store/consts";
 import { NextResponse } from "next/server";
 
 export async function POST(request: Request): Promise<NextResponse> {
+    const twitterSecret = process.env.TWITTER_CLIENT_SECRET;
+    if (!twitterSecret) {
+        console.error("TWITTER_CLIENT_SECRET was not set");
+        return NextResponse.json({}, { status: 500 });
+    }
+
     const { code, challenge } = await request.json();
     if (typeof code !== "string" || typeof challenge !== "string") {
         return NextResponse.json({}, { status: 400 });
@@ -13,7 +19,7 @@ export async function POST(request: Request): Promise<NextResponse> {
         redirect_uri: new URL("/twitter-id", request.url).toString(),
         code_verifier: challenge,
     });
-    const auth = `${TWITTER_CLIENT_ID}:${process.env.TWITTER_CLIENT_SECRET}`;
+    const auth = `${TWITTER_CLIENT_ID}:${twitterSecret}`;
     const tokenRes = await fetch("https://api.twitter.com/2/oauth2/token", {
         method: "POST",
         headers: {
