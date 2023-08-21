960ad541d09455a7c867a1eb63c8ce13b5c7d45d
MikuroXina <ryosukadnak@gmail.com>
2023-08-21 18:57:34 +0900

    Add github-me endpoint

---
+++ src/app/github-me/route.ts
@@ -0,0 +1,15 @@
+import { NextResponse } from "next/server";
+
+export async function GET(request: Request): Promise<NextResponse> {
+    const auth = request.headers.get("Authorization");
+    if (!auth || !auth.startsWith("Bearer ")) {
+        return NextResponse.json({}, { status: 401 });
+    }
+
+    const res = await fetch("https://api.github.com/user", {
+        headers: {
+            Authorization: auth,
+        },
+    });
+    return NextResponse.json(await res.json(), { status: res.status });
+}
