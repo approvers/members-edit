d47681c27077ee3fd22bf3e176f6392a204c2c48
MikuroXina <mikuroxina@gmail.com>
2024-08-25 22:34:57 +0900

    Sort imports

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,9 @@
 import {
-    vitePlugin as remix,
     cloudflareDevProxyVitePlugin as remixCloudflareDevProxy,
+    vitePlugin as remix,
 } from "@remix-run/dev";
-import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 export default defineConfig({
     plugins: [
