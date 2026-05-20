6cb197c1310858ed299e33c702cd13d5768c00a6
Mikuroさいな <10331164+MikuroXina@users.noreply.github.com>
2026-05-21 08:54:25 +0900

    chore: Migrate vite-tsconfig-paths (#419)

--- vite.config.ts
+++ vite.config.ts
@@ -3,15 +3,16 @@ import { cloudflareDevProxy } from "@react-router/dev/vite/cloudflare";
 import tailwindCss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
-import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
     plugins: [
         tailwindCss(),
         cloudflareDevProxy(),
         !process.env.VITEST ? reactRouter() : react(),
-        tsconfigPaths(),
     ],
+    resolve: {
+        tsconfigPaths: true,
+    },
     server: {
         port: 3000,
     },
