9f95559fbd7fd1a3ac2824de44f0234f706de448
MikuroXina <10331164+MikuroXina@users.noreply.github.com>
2026-06-21 22:38:21 +0900

    Replace with @cloudflare/vite-plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
+import { cloudflare } from "@cloudflare/vite-plugin";
 import { reactRouter } from "@react-router/dev/vite";
-import { cloudflareDevProxy } from "@react-router/dev/vite/cloudflare";
 import tailwindCss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
@@ -7,7 +7,7 @@ import { defineConfig } from "vite";
 export default defineConfig({
     plugins: [
         tailwindCss(),
-        cloudflareDevProxy(),
+        cloudflare(),
         !process.env.VITEST ? reactRouter() : react(),
     ],
     resolve: {
